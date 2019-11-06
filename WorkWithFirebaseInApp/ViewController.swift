bfeac2e93d6141c288489a2a8df019761e85f7a6
Josh Kinney <joshkinney@kinneykare.com>
2019-11-05 19:17:06 -0600

    Initial Commit

---
+++ WorkWithFirebaseInApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkWithFirebaseInApp
+//
+//  Created by Kinney Kare on 11/5/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
